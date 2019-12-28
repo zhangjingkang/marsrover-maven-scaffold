bdebf0529af9e2d95206720a735a65fe8857308f
zhangjingkang <zhangjingkang@gmail.com>
2019-12-28 15:59:53 +0800

    red01

---
+++ src/main/java/Area/Area.java
@@ -0,0 +1,7 @@
+package Area;
+
+public class Area {
+    public Area(int i, int i1) {
+
+    }
+}
---
+++ src/main/java/Rover/Rover.java
@@ -0,0 +1,19 @@
+package Rover;
+
+import Area.Area;
+
+public class Rover {
+    public static final String EAST = "EAST";
+
+    public String land(Area area, int i, int i1, String director) {
+        return "";
+    }
+
+    public String move() {
+        return "";
+    }
+
+    public String getPosition() {
+        return "";
+    }
+}
